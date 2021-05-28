31ad0b0a037a66fbf8e063cc998a0fefef8bdcca
Joe Porrazzo <joepa2624@me.com>
2021-05-27 18:50:52 -0700

    connected to mysql

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+const mysql = require('mysql2');
 const express = require('express');
 
 const PORT = process.env.PORT || 3001;
@@ -7,9 +8,26 @@ const app = express();
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
+// connect to database
+const db = mysql.createConnection(
+    {
+        host: 'localhost',
+        // your mysql username,
+        user: 'root', 
+        // your mysql password
+        password: '_4nmdXeTFDKQ$i)WS.',
+        database: 'election'
+    },
+    console.log('Connected to the election database')
+);
+
 // Default response for any other request (not found)
 app.use((req, res) => {
     res.status(404).end();
+});
+
+db.query('SELECT * FROM candidates', (err, rows) => {
+    console.log(rows);
 });
 
 app.listen(PORT, () => {
