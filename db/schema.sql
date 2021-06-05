2b148728332edd09526d4545368d79bdb90cfb4d
Joe Porrazzo <joepa2624@me.com>
2021-06-05 15:42:55 -0700

    created votes table

--- db/schema.sql
+++ db/schema.sql
@@ -1,6 +1,9 @@
+
+DROP TABLE IF EXISTS votes;
 DROP TABLE IF EXISTS candidates;
 DROP TABLE IF EXISTS parties;
 DROP TABLE IF EXISTS voters;
+
 
 CREATE TABLE parties (
   id INTEGER AUTO_INCREMENT PRIMARY KEY,
@@ -23,3 +26,14 @@ CREATE TABLE voters (
   last_name VARCHAR(30) NOT NULL,
   email VARCHAR(50) NOT NULL,
   created_at DATETIME DEFAULT CURRENT_TIMESTAMP
+);
+
+  CREATE TABLE votes (
+    id INTEGER AUTO_INCREMENT PRIMARY KEY,
+    voter_id INTEGER NOT NULL,
+    candidate_id INTEGER NOT NULL,
+    create_at DATETIME DEFAULT CURRENT_TIMESTAMP,
+    CONSTRAINT uc_voter UNIQUE (voter_id),  -- values inserted in the voter_id field must be unique
+    CONSTRAINT fk_voter FOREIGN KEY (voter_id) REFERENCES  voters(id) ON DELETE CASCADE,
+    CONSTRAINT fk_candidate FOREIGN KEY (candidate_id) REFERENCES candidates(id) ON DELETE CASCADE -- deletes the entire row from this table
+  );
